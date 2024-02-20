ad5c5efd196032a3a6d23df75fa1b862a0084a44
Ashraf mahmud <ashraf.islam@parallaxlogic.net>
2024-02-20 21:34:42 +0600

    Completed The Project

--- src/Components/Wedding/Weddings.jsx
+++ src/Components/Wedding/Weddings.jsx
@@ -2,7 +2,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 const Weddings = () => {
     return (
         <div className="mx-auto relative">
-            <div><img className="w-full h-[750px]" src="https://img.freepik.com/free-photo/medium-shot-married-couple-kissing_23-2149956436.jpg?size=626&ext=jpg&uid=R138265752&ga=GA1.1.22933237.1708163458&semt=ais" alt="" /></div>
+            <div><img className="w-full h-[750px] overflow-hidden relative" src="https://img.freepik.com/free-photo/medium-shot-married-couple-kissing_23-2149956436.jpg?size=626&ext=jpg&uid=R138265752&ga=GA1.1.22933237.1708163458&semt=ais" alt="" /></div>
             <div className="absolute lg:left-[680px]  w-[610px] h-[600px] rounded-full px-10 -mt-40  bg-white">
                 <div className="z-10 space-y-10 text-center pt-24">
                     <p className="text-xl text-[#99A88A]">VENUES</p>
